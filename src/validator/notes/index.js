0b5aba052b11842ce198d6e41e6a281fab00d380
hasanmubarok9 <hasanmubarok32@gmail.com>
2025-08-24 02:09:20 +0700

    update notes

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -1,3 +1,5 @@
+const { NotePayloadSchema } = require('./schema');
+
 const NotesValidator = {
   validateNotePayload: (payload) => {
     const validationResult = NotePayloadSchema.validate(payload);
