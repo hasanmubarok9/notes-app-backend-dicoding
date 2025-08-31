5a2f136af05309f0ad1a4ca215bff392213d9c1d
hasanmubarok9 <hasanmubarok32@gmail.com>
2025-08-31 22:00:52 +0700

    init add user

---
+++ src/services/postgres/UserService.js
@@ -0,0 +1,31 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+const InvariantError = require('../../exceptions/InvariantError.js');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.has(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+}
+
+module.exports = UsersService;
