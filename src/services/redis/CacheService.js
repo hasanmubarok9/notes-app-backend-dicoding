d8ca704d18e926c4a5438bc95aad93c80107d122
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-11-12 13:40:10 +0700

    implement function get, set, and delete on cache service

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -12,4 +12,27 @@ class CacheService {
 
     this._client.connect();
   }
+
+
+  async set(key, value, expirationInSecond = 3600) {
+    await this._client.set(key, value, {
+      EX: expirationInSecond,
+    });
+  }
+
+  async get(key) {
+    const result = await this._client.get(key);
+
+    if (result === null) {
+      throw new Error('Cache tidak ditemukan');
+    }
+
+    return result;
+  }
+
+  delete(key) {
+    return this._client.del(key);
+  }
 }
+
+module.exports = CacheService;
